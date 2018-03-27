b0d54c237e4c89b87d2138f7538243a3fe5813bb
Tomek Piasecki <netpiaseq+github@gmail.com>
2018-03-27 16:46:59 +0200

    adds unit tests for Skills

--- tests/unit/Domain/Skills/MagicShieldTest.php
+++ tests/unit/Domain/Skills/MagicShieldTest.php
@@ -6,11 +6,61 @@ namespace Tpiasecki\HeroGameTest\unit\Domain\Skills;
 
 use PHPUnit\Framework\TestCase;
 use Tpiasecki\HeroGame\Domain\Skills\MagicShield;
+use Tpiasecki\HeroGame\Infrastructure\RandomnessProviderInterface;
 
 class MagicShieldTest extends TestCase
 {
-    public function testIsCreatedCorrectly()
+    public function testShouldAttackAgainReturnsFalseIfProbabilityConditionIsMet(): void
     {
-        $this->assertInstanceOf(MagicShield::class, new MagicShield());
+        $this->assertFalse($this->getInstance(15)->shouldAttackAgain());
+    }
+
+    public function testShouldAttackAgainReturnsFalseIfProbabilityConditionIsNotMet(): void
+    {
+        $this->assertFalse($this->getInstance(50)->shouldAttackAgain());
+    }
+
+    /**
+     * @dataProvider getDamageValues
+     */
+    public function testDamageGetsReducedIfProbabilityConditionIsMet(int $damage): void
+    {
+        $expected = (int) ($damage / 2);
+        $result = $this->getInstance(10)->reduceDamage($damage);
+        $this->assertSame($expected, $result);
+    }
+
+    /**
+     * @dataProvider getDamageValues
+     */
+    public function testDamageIsNotReducedIfProbabilityConditionIsNotMet(int $damage): void
+    {
+        $this->assertSame($damage, $this->getInstance(50)->reduceDamage($damage));
+    }
+
+    public function getDamageValues(): array
+    {
+        return [
+            [5],
+            [9],
+            [13],
+            [18],
+            [25],
+            [33],
+            [45]
+        ];
+    }
+
+    public function testNameIsNotEmpty(): void
+    {
+        $this->assertNotEmpty($this->getInstance()->getName());
+    }
+
+    private function getInstance($luckyDrawValue = 0): MagicShield
+    {
+        $randomnessProvider = $this->prophesize(RandomnessProviderInterface::class);
+        $randomnessProvider->randomInt(1, 100)->willReturn($luckyDrawValue);
+        $randomnessProvider = $randomnessProvider->reveal();
+        return new MagicShield($randomnessProvider);
     }
 }
--- tests/unit/Domain/Skills/RapidStrikeTest.php
+++ tests/unit/Domain/Skills/RapidStrikeTest.php
@@ -6,11 +6,61 @@ namespace Tpiasecki\HeroGameTest\unit\Domain\Skills;
 
 use PHPUnit\Framework\TestCase;
 use Tpiasecki\HeroGame\Domain\Skills\RapidStrike;
+use Tpiasecki\HeroGame\Infrastructure\RandomnessProviderInterface;
 
 class RapidStrikeTest extends TestCase
 {
-    public function testIsCreatedCorrectly()
+    /**
+     * @dataProvider getDamageValues
+     * @param $damage
+     */
+    public function testReduceDamageDoesntChangeValueIfProbabilityConditionIsMet(int $damage): void
     {
-        $this->assertInstanceOf(RapidStrike::class, new RapidStrike());
+        $this->assertSame($damage, $this->getInstance(5)->reduceDamage($damage));
+    }
+
+    /**
+     * @dataProvider getDamageValues
+     * @param $damage
+     */
+    public function testReduceDamageDoesntChangeValueIfProbabilityConditionIsNotMet(int $damage): void
+    {
+        $this->assertSame($damage, $this->getInstance(30)->reduceDamage($damage));
+    }
+
+    public function testShouldAttackAgainReturnsTrueIfProbabilityConditionIsMet(): void
+    {
+        $this->assertTrue($this->getInstance(8)->shouldAttackAgain());
+    }
+
+    public function testShouldAttackAgainReturnsFalseIfProbabilityConditionIsNotMet(): void
+    {
+        $this->assertFalse($this->getInstance(33)->shouldAttackAgain());
+    }
+
+    public function getDamageValues(): array
+    {
+        return [
+            [5],
+            [9],
+            [13],
+            [18],
+            [25],
+            [33],
+            [45]
+        ];
+    }
+
+    public function testNameIsNotEmpty(): void
+    {
+        $this->assertNotEmpty($this->getInstance()->getName());
+    }
+
+    private function getInstance($luckyDrawValue = 0): RapidStrike
+    {
+        $randomnessProvider = $this->prophesize(RandomnessProviderInterface::class);
+        $randomnessProvider->randomInt(1, 100)->willReturn($luckyDrawValue);
+        $randomnessProvider = $randomnessProvider->reveal();
+        return new RapidStrike($randomnessProvider);
     }
 }
